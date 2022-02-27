64b7a3a66e59d130141d2344a20b5c7a67de6e02
amitai.getzler <amitai.getzler@explorium.ai>
2022-02-27 20:23:09 +0200

    update

--- src/app/components/flow-viz/flow-viz.component.ts
+++ src/app/components/flow-viz/flow-viz.component.ts
@@ -14,39 +14,87 @@ export class FlowVizComponent {
   public links: Edge[] = [
     {
       id: 'a',
-      source: 'first',
-      target: 'second',
+      source: '1',
+      target: '2',
       label: 'is parent of'
     },
     {
       id: 'b',
-      source: 'first',
-      target: 'third',
+      source: '1',
+      target: '3',
       label: 'custom label'
     },
     {
       id: 'c',
-      source: 'first',
-      target: 'fourth',
+      source: '1',
+      target: '4',
       label: 'custom label'
     }
   ];
   public nodes: Node[] = [
     {
-      id: 'first',
+      id: '1',
       label: 'A'
     },
     {
-      id: 'second',
+      id: '2',
       label: 'B'
     },
     {
-      id: 'third',
+      id: '3',
       label: 'C'
     },
     {
-      id: 'fourth',
+      id: '4',
       label: 'D'
     }
   ];
+  ngOnInit(){
+    this.nodes.forEach(node=>{
+      this.nodes.push({id:`plus_${node.id}`,label:"+"})
+      this.nodes = [...this.nodes];
+      this.links.push({
+        id:`plus_${node.id}`,
+        source: node.id,
+        target: `plus_${node.id}`,
+        label: '+'
+      })
+      this.links = [...this.links];
+    })
+  }
+
+  onNodeClick(event){
+    console.log(event)
+  }
+  onNodeDblClick(event){
+    console.log(event)
+  }
+  onPlusClick(event){
+    const id = event.id.split("_").slice(1).join("_")
+    const num = btoa(Math.random().toString()).substr(10, 5);
+
+    this.nodes.push({id:`${num}_${id}`,label:num})
+
+    this.nodes.push({id:`plus_${num}_${id}`,label:"+"})
+
+    this.nodes = [...this.nodes];
+
+    this.links.push({
+      id:`${num}_${id}`,
+      source: id,
+      target: `${num}_${id}`,
+      label: num
+    })
+
+    this.links.push({
+      id:`plus_${num}_${id}`,
+      source: `${num}_${id}`,
+      target: `plus_${num}_${id}`,
+      label: '+'
+    })
+
+    console.log(`Pushing Node ${num}_${id} to be connected to ${id}. Giving it plus_${num}_${id}, it will be connected to ${num}_${id}`)
+
+    this.links = [...this.links];
+  }
 }

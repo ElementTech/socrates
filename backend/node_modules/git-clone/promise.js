9443602604c2de84f5dbc03a218af32d30e1c528
amitai.getzler <amitai.getzler@explorium.ai>
2022-03-20 10:51:41 +0200

    upload

--- backend/node_modules/git-clone/promise.js
+++
@@ -1,7 +0,0 @@
-const impl = require('./private/impl');
-
-module.exports = function(repo, targetPath, opts) {
-    return new Promise((yes, no) => {
-        impl(repo, targetPath, opts || {}, yes, no);
-    });
-}
